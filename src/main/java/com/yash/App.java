4125397c42ea5bcace9e6a684aef593c4813a395
Yash Sahay <yashsahay999@gmail.com>
2025-09-01 22:37:23 +0530

    Added code to insert, update and delete student records

--- src/main/java/com/yash/App.java
+++ src/main/java/com/yash/App.java
@@ -1,10 +1,31 @@
 package com.yash;
 
+import com.yash.dao.StudentDao;
+import com.yash.model.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App
 {
     public static void main( String[] args ) {
 
+        final ApplicationContext context = new ClassPathXmlApplicationContext(
+                "config.xml");
 
+        final StudentDao studentDao = context.getBean("studentDaoImpl", StudentDao.class);
+
+        int recordsUpdated = 0;
+
+        final Student student = new Student("Yash", "Sasaram");
+        recordsUpdated = studentDao.insert(student);
+        System.out.println(recordsUpdated + " record(s) inserted!");
+
+        student.setCity("Hyderabad");
+        recordsUpdated = studentDao.updateById(1, student);
+        System.out.println(recordsUpdated + " record(s) updated!");
+
+        recordsUpdated = studentDao.deleteById(1);
+        System.out.println(recordsUpdated + " record(s) deleted!");
 
     }
 }
